19d0d2c6963b9a717ee510ea71b3050aca4296f6
furst <andreas.furst@gmail.com>
2020-05-27 20:54:59 +0200

    project path

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,6 +16,8 @@ if [ -z "${PROJECT_PATH}" ]; then
   PROJECT_PATH="."
 fi
 
+echo ${PROJECT_PATH}
+
 # Go can only find dependencies if they're under $GOPATH/src.
 # GitHub Actions mounts your repository outside of $GOPATH.
 # So symlink the repository into $GOPATH, and then cd to it.
